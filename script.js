b12dee7199655c4faafda16e312a9683c626f117
WIZENIT <166426832+WIZENIT@users.noreply.github.com>
2025-06-19 20:42:48 -0300

    Update script.js melhor desenvolvimento do som

--- script.js
+++ script.js
@@ -2,20 +2,56 @@ document.addEventListener('DOMContentLoaded', function() {
     const slider = document.querySelector('.slider');
     const prevArrow = document.querySelector('.prev-arrow');
     const nextArrow = document.querySelector('.next-arrow');
-    
-    // Configura o evento de clique para a seta anterior
     prevArrow.addEventListener('click', function() {
         slider.scrollBy({
             left: -slider.offsetWidth * 0.8,
             behavior: 'smooth'
         });
     });
-    
-    // Configura o evento de clique para a próxima seta
     nextArrow.addEventListener('click', function() {
         slider.scrollBy({
             left: slider.offsetWidth * 0.8,
             behavior: 'smooth'
         });
     });
-});
+}); 
+    //reggae
+    function reggae1() {
+    var audio = document.getElementById("edson");
+    audio.play();
+    }
+    function reggae1stop() {
+    const audio = document.getElementById("edson");
+    audio.pause();
+    audio.currentTime = 0;
+    }
+    function play() {
+    var audio = document.getElementById("audio");
+    audio.play();
+    }
+    function play() {
+    var audio = document.getElementById("audio");
+    audio.play();
+    }
+    function play() {
+    var audio = document.getElementById("audio");
+    audio.play();
+    }
+    function play() {
+    var audio = document.getElementById("audio");
+    audio.play();
+    }
+    function play() {
+    var audio = document.getElementById("audio");
+    audio.play();
+    }
+    function play() {
+    var audio = document.getElementById("audio");
+    audio.play();
+    }
+    function play() {
+    var audio = document.getElementById("audio");
+    audio.play();
+    }
+
+    //
